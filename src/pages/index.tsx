aad36c5ea6f5efe1d0a846380120d239613f26de
jesteban39 <juanesqb28@gmail.com>
2023-04-06 23:44:35 -0500

    add login tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import styles from '@app/styles/Home.module.css';
-import Login from '@app/pages/components/Login/Login';
+import Login from '@app/pages/components/Login';
 
 const inter = Inter({ subsets: ['latin'] })
 
